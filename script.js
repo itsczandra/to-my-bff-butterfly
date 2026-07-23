46ec143a825531d4e03329ad7909f6e2ea45c7e3
Cz <caz.chumsters@gmail.com>
2026-07-23 12:11:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function showLetter(){
+const reveals = document.querySelectorAll(".reveal");
 
-    document
-        .getElementById("letter")
-        .scrollIntoView({
-            behavior:"smooth"
-        });
+window.addEventListener("scroll", reveal);
+
+reveal();
+
+function reveal(){
+
+    reveals.forEach(section=>{
+
+        const top = section.getBoundingClientRect().top;
+
+        const trigger = window.innerHeight - 120;
+
+        if(top < trigger){
+
+            section.classList.add("active");
+
+        }
+
+    });
 
 }
